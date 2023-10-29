402364c68e684876cf13e88cfe2c8cd0146886f8
Philipp Toepfer <p.toepfer@fm-maschinenbau.de>
2023-10-29 19:53:21 +0100

    Add password hashing

--- src/controllers/auth/service/prisma.auth.service.ts
+++ src/controllers/auth/service/prisma.auth.service.ts
@@ -1,7 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 import { PrismaService } from 'src/persistence/prisma/prisma.service';
 import { ICreateUserDto } from '../types/ICreateUserDto';
 import { ILoginUserDto } from '../types/ILoginUserDto';
+import { IPersistedUser } from '../types/IPersistedUser';
+import { IUserResponseDto } from '../types/IUserResponseDto';
 import { IAuthService } from './interface/IAuthService';
 
 @Injectable()
@@ -9,12 +17,45 @@ export class PrismaAuthService implements IAuthService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async register(createUserDto: ICreateUserDto) {
-    const user = await this.prismaService.user.create(createUserDto);
-    return user;
+    const userToCreate = await toUserWithHashedPassword(createUserDto);
+    try {
+      const persistedUser = await this.prismaService.user.create(userToCreate);
+      return toUserResponse(persistedUser);
+    } catch (e) {
+      throw new ConflictException('Email already exists.');
+    }
   }
 
   async login(loginUserDto: ILoginUserDto) {
     const user = await this.prismaService.user.findByEmail(loginUserDto.email);
-    return user;
+    if (!user) {
+      throw new NotFoundException(
+        `User not found for email: ${loginUserDto.email}`,
+      );
+    }
+    const passwordIsValid = await bcrypt.compare(
+      loginUserDto.password,
+      user.password,
+    );
+
+    if (!passwordIsValid) {
+      throw new UnauthorizedException('Invalid password');
+    }
+    return toUserResponse(user);
   }
 }
+
+const toUserWithHashedPassword = async (
+  user: ICreateUserDto,
+): Promise<ICreateUserDto> => {
+  const hashedPassword = await bcrypt.hash(user.password, 10);
+  return { ...user, password: hashedPassword };
+};
+
+const toUserResponse = (user: IPersistedUser): IUserResponseDto => {
+  return {
+    email: user.email,
+    id: user.id,
+    name: user.name,
+  };
+};
---
+++ src/controllers/auth/types/IPersistedUser.ts
@@ -0,0 +1,6 @@
+export interface IPersistedUser {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+}
